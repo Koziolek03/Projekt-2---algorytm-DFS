2cf8dd55082e7c262000f217aa4dfe358a7a7c5c
Jakub Kozłowski - laptop <s197623@student.pg.edu.pl>
2024-03-26 17:24:26 +0100

    podstawowe menu i szkic funckji

--- Projekt 2 - algorytm DFS/Projekt 2 - algorytm DFS.cpp
+++ Projekt 2 - algorytm DFS/Projekt 2 - algorytm DFS.cpp
@@ -1,26 +1,59 @@
-﻿
-#include <iostream>
-
+﻿#include <iostream>
+#include <vector>
 using namespace std;
+
+struct wektory {
+    double value;
+    bool visited;
+    vector <int> buddy;
+};
+
+    void wybranie_grafu() {
+        cout << "1";
+    };
+    void dodaj_do_grafu() {
+        cout << "2";
+    };
+    void usun_z_grafu() {
+        cout << "3";
+    };
 
 void menu() {
 
-    cout << "to jest menu";
+    int choice; 
 
-    cout << "/topcja 3 - dodaj wierzchołek";
+    cout << "1. Wybieranie grafu\n";
+    cout << "2. Dodawanie do grafu\n";
+    cout << "3. Usuwanie z grafu\n";
+
+    cin >> choice; 
+
+    switch (choice) {
+    case 1:
+        wybranie_grafu();
+        break;
+
+    case 2:
+
+        dodaj_do_grafu();
+        break;
+
+    case 3:
+
+        usun_z_grafu();
+        break;
+
+    }
     
-    cout << "super test";
-
 }
 
-void wektor() {
 
-    cout << "nowy wektor";
-}
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "Algorytm DFS przeszukujacy graf \n";
+    cout << "Made by Jakub Kozlowski 197623 & Szymon Mielniczak 197956\n";
+
     menu();
-    wektor();
+   
 }
 
