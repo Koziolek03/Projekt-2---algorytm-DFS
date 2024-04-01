b60840cf1eef361150d6217840766c5dbd4c58c4
Szymon Mielniczak <jmarian600@gmail.com>
2024-04-01 21:10:42 +0200

    dodanie opcji default w menu, gdy wprowadzony zostanie neiwłasciwy numer

--- Projekt 2 - algorytm DFS/Projekt 2 - algorytm DFS.cpp
+++ Projekt 2 - algorytm DFS/Projekt 2 - algorytm DFS.cpp
@@ -10,9 +10,10 @@ struct wektory {
 };
 
 void wybranie_grafu1() {
-    cout << "Wybrano graf 1" << endl;
+    cout << "\nWybrano graf 1" << endl;
 
-    wektory w1 = { 12, 0, {2, 3, 6, 7, 8} };
+    wektory w0 = {  };
+    wektory w1 = { 12.5, 0, {2, 3, 6, 7, 8} };
     wektory w2 = { 18, 0, {1, 3, 4, 8} };
     wektory w3 = { 24, 0, {1, 2, 6, 8} };
     wektory w4 = { 3, 0, {2} };
@@ -21,9 +22,9 @@ void wybranie_grafu1() {
     wektory w7 = { 2137, 0, {1} };
     wektory w8 = { 123, 0, {1, 2, 3} };
 
-    vector<wektory> graf = { w1, w2, w3, w4, w5, w6, w7, w8 };
+    vector<wektory> graf = { w0, w1, w2, w3, w4, w5, w6, w7, w8 };
 
-    for (int i = 0; i < graf.size(); ++i) {
+    for (int i = 1; i < graf.size(); ++i) {
         cout << "Wierzcholek "<< i <<", wartosc: " << graf[i].value << ", sasiedzi: ";
         for (int sasiad : graf[i].buddy) {
             cout << sasiad << ",";
@@ -32,8 +33,9 @@ void wybranie_grafu1() {
     }
 }
 void wybranie_grafu2() {
-    cout << "Wybrano graf 2" << endl;
+    cout << "\nWybrano graf 2" << endl;
 
+    wektory w0 = {  };
     wektory w1 = { 112, 0, {2,3,5} };
     wektory w2 = { 198, 0, {1,4,6} };
     wektory w3 = { 264, 0, {1,5} };
@@ -41,9 +43,9 @@ void wybranie_grafu2() {
     wektory w5 = { 362, 0, {1,3,6} };
     wektory w6 = { 535, 0, {2,4,5} };
 
-    vector<wektory> graf = { w1, w2, w3, w4, w5, w6 };
+    vector<wektory> graf = { w0, w1, w2, w3, w4, w5, w6 };
 
-    for (int i = 0; i < graf.size(); ++i) {
+    for (int i = 1; i < graf.size(); ++i) {
         cout << "Wierzcholek " << i << ", wartosc: " << graf[i].value << ", sasiedzi: ";
         for (int sasiad : graf[i].buddy) {
             cout << sasiad << ",";
@@ -52,8 +54,9 @@ void wybranie_grafu2() {
     }
 }
 void wybranie_grafu3() {
-    cout << "Wybrano graf 3" << endl;
+    cout << "\nWybrano graf 3" << endl;
 
+    wektory w0 = {  };
     wektory w1 = { 121, 0, {4,9} };
     wektory w2 = { 184, 0, {3,10} };
     wektory w3 = { 224, 0, {2,4,6,8} };
@@ -65,9 +68,9 @@ void wybranie_grafu3() {
     wektory w9 = { 457, 0, {1,10} };
     wektory w10 = { 1233, 0, {2,9} };
 
-    vector<wektory> graf = { w1, w2, w3, w4, w5, w6, w7, w8, w9, w10 };
+    vector<wektory> graf = { w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10 };
 
-    for (int i = 0; i < graf.size(); ++i) {
+    for (int i = 1; i < graf.size(); ++i) {
         cout << "Wierzcholek " << i << ", wartosc: " << graf[i].value << ", sasiedzi: ";
         for (int sasiad : graf[i].buddy) {
             cout << sasiad << ",";
@@ -88,36 +91,38 @@ void wybranie_grafu3() {
         bool exit = 0;
         int choice;
         while(!exit){
-        cout << "1. Wybieranie grafu 1\n";
-        cout << "2. Wybieranie grafu 2\n";
-        cout << "3. Wybieranie grafu 3\n";
-        cout << "4. Dodawanie do grafu\n";
-        cout << "5. Usuwanie z grafu\n";
-        cout << "6. exit\n";
+            cout << "\n1. Wybieranie grafu 1\n";
+            cout << "2. Wybieranie grafu 2\n";
+            cout << "3. Wybieranie grafu 3\n";
+            cout << "4. Dodawanie do grafu\n";
+            cout << "5. Usuwanie z grafu\n";
+            cout << "6. exit\n";
 
-        cin >> choice;
-
-        switch (choice) {
-        case 1:
-            wybranie_grafu1();
-            break;
-        case 2:
-            wybranie_grafu2();
-            break;
-        case 3:
-            wybranie_grafu3();
-            break;
-        case 4:
-            dodaj_do_grafu();
-            break;
-        case 5:
-            usun_z_grafu();
-            break;
-        case 6:
-            exit = 1;
-            break;
+            cin >> choice;
+            switch (choice) {
+            case 1:
+                wybranie_grafu1();
+                break;
+            case 2:
+                wybranie_grafu2();
+                break;
+            case 3:
+                wybranie_grafu3();
+                break;
+            case 4:
+                dodaj_do_grafu();
+                break;
+            case 5:
+                usun_z_grafu();
+                break;
+            case 6:
+                exit = 1;
+                break;
+            default:
+                cout << "\n\tNie ma takiej funkcji." << endl;
+                break;
+            }
         }
-    }
     }
 
     int main()
