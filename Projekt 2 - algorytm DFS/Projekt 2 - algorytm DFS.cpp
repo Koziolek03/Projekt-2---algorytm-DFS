9cbf2c9ade6cb14ae49465f2ae2585fe779c953d
Jakub Kozłowski - laptop <s197623@student.pg.edu.pl>
2024-04-02 21:32:31 +0200

    reorganizacja "szkieletu" programu, dodanie grafu jako zmienna w main, dodanie wierzchołka 0

--- Projekt 2 - algorytm DFS/Projekt 2 - algorytm DFS.cpp
+++ Projekt 2 - algorytm DFS/Projekt 2 - algorytm DFS.cpp
@@ -3,25 +3,25 @@
 #include <vector>
 using namespace std;
 
-struct wektory {
+struct node {
     double value;
     bool visited;
     vector <int> buddy;
 };
 
-void wybranie_grafu1() {
+void wybranie_grafu1(vector<node>& graf) {
     cout << "Wybrano graf 1" << endl;
+    node w0 = { 0,0,{1} };
+    node w1 = { 12, 0, {2, 3, 6, 7, 8} };
+    node w2 = { 18, 0, {1, 3, 4, 8} };
+    node w3 = { 24, 0, {1, 2, 6, 8} };
+    node w4 = { 3, 0, {2} };
+    node w5 = { 62, 0, {3} };
+    node w6 = { 55, 0, {1} };
+    node w7 = { 2137, 0, {1} };
+    node w8 = { 123, 0, {1, 2, 3} };
 
-    wektory w1 = { 12, 0, {2, 3, 6, 7, 8} };
-    wektory w2 = { 18, 0, {1, 3, 4, 8} };
-    wektory w3 = { 24, 0, {1, 2, 6, 8} };
-    wektory w4 = { 3, 0, {2} };
-    wektory w5 = { 62, 0, {3} };
-    wektory w6 = { 55, 0, {1} };
-    wektory w7 = { 2137, 0, {1} };
-    wektory w8 = { 123, 0, {1, 2, 3} };
-
-    vector<wektory> graf = { w1, w2, w3, w4, w5, w6, w7, w8 };
+    graf = { w1, w2, w3, w4, w5, w6, w7, w8 };
 
     for (int i = 0; i < graf.size(); ++i) {
         cout << "Wierzcholek "<< i <<", wartosc: " << graf[i].value << ", sasiedzi: ";
@@ -31,17 +31,17 @@ void wybranie_grafu1() {
         cout << endl;
     }
 }
-void wybranie_grafu2() {
+void wybranie_grafu2(vector<node>& graf) {
     cout << "Wybrano graf 2" << endl;
+    node w0 = { 0,0,{1} };
+    node w1 = { 112, 0, {2,3,5} };
+    node w2 = { 198, 0, {1,4,6} };
+    node w3 = { 264, 0, {1,5} };
+    node w4 = { 355, 0, {2,6} };
+    node w5 = { 362, 0, {1,3,6} };
+    node w6 = { 535, 0, {2,4,5} };
 
-    wektory w1 = { 112, 0, {2,3,5} };
-    wektory w2 = { 198, 0, {1,4,6} };
-    wektory w3 = { 264, 0, {1,5} };
-    wektory w4 = { 355, 0, {2,6} };
-    wektory w5 = { 362, 0, {1,3,6} };
-    wektory w6 = { 535, 0, {2,4,5} };
-
-    vector<wektory> graf = { w1, w2, w3, w4, w5, w6 };
+    graf = { w1, w2, w3, w4, w5, w6 };
 
     for (int i = 0; i < graf.size(); ++i) {
         cout << "Wierzcholek " << i << ", wartosc: " << graf[i].value << ", sasiedzi: ";
@@ -51,21 +51,21 @@ void wybranie_grafu2() {
         cout << endl;
     }
 }
-void wybranie_grafu3() {
+void wybranie_grafu3(vector<node>& graf) {
     cout << "Wybrano graf 3" << endl;
+    node w0 = { 0,0,{1} };
+    node w1 = { 121, 0, {4,9} };
+    node w2 = { 184, 0, {3,10} };
+    node w3 = { 224, 0, {2,4,6,8} };
+    node w4 = { 31, 0, {1,3,7} };
+    node w5 = { 62, 0, {7,8} };
+    node w6 = { 55, 0, {3,8} };
+    node w7 = { 2137, 0, {4,5,8} };
+    node w8 = { 56, 0, {3,5,6,7} };
+    node w9 = { 457, 0, {1,10} };
+    node w10 = { 1233, 0, {2,9} };
 
-    wektory w1 = { 121, 0, {4,9} };
-    wektory w2 = { 184, 0, {3,10} };
-    wektory w3 = { 224, 0, {2,4,6,8} };
-    wektory w4 = { 31, 0, {1,3,7} };
-    wektory w5 = { 62, 0, {7,8} };
-    wektory w6 = { 55, 0, {3,8} };
-    wektory w7 = { 2137, 0, {4,5,8} };
-    wektory w8 = { 56, 0, {3,5,6,7} };
-    wektory w9 = { 457, 0, {1,10} };
-    wektory w10 = { 1233, 0, {2,9} };
-
-    vector<wektory> graf = { w1, w2, w3, w4, w5, w6, w7, w8, w9, w10 };
+    graf = { w1, w2, w3, w4, w5, w6, w7, w8, w9, w10 };
 
     for (int i = 0; i < graf.size(); ++i) {
         cout << "Wierzcholek " << i << ", wartosc: " << graf[i].value << ", sasiedzi: ";
@@ -76,18 +76,18 @@ void wybranie_grafu3() {
     }
 }
 
-    void dodaj_do_grafu() {
+    void dodaj_do_grafu(vector<node>& graf) {
         cout << "2";
     };
-    void usun_z_grafu() {
+    void usun_z_grafu(vector<node>& graf) {
         cout << "3";
     };
 
 
-    void menu() {
-        bool exit = 0;
+    void menu(bool &exit,vector<node>&graf) {
+        
         int choice;
-        while(!exit){
+        
         cout << "1. Wybieranie grafu 1\n";
         cout << "2. Wybieranie grafu 2\n";
         cout << "3. Wybieranie grafu 3\n";
@@ -99,33 +99,39 @@ void wybranie_grafu3() {
 
         switch (choice) {
         case 1:
-            wybranie_grafu1();
+            wybranie_grafu1(graf);
             break;
         case 2:
-            wybranie_grafu2();
+            wybranie_grafu2(graf);
             break;
         case 3:
-            wybranie_grafu3();
+            wybranie_grafu3(graf);
             break;
         case 4:
-            dodaj_do_grafu();
+            dodaj_do_grafu(graf);
             break;
         case 5:
-            usun_z_grafu();
+            usun_z_grafu(graf);
             break;
         case 6:
             exit = 1;
             break;
         }
     }
-    }
+    
 
     int main()
     {
+        bool exit = 0;
+        vector<node>graf;
     cout << "Algorytm DFS przeszukujacy graf \n";
     cout << "Made by Jakub Kozlowski 197623 & Szymon Mielniczak 197956\n";
 
-    menu();
-   
+
+    while (!exit) {
+        menu(exit,graf);
+    }
+
+    //algorytm dfs
 }
 
